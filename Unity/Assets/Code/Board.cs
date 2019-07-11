41f1b93438000676af97b2a276a591cb8e22be57
Léo Garreau <leo.gar@hotmail.fr>
2019-07-11 17:37:54 +0200

    board and players initialized

---
+++ Unity/Assets/Code/Board.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Code
+{
+    public class Board : MonoBehaviour
+    {
+        [HideInInspector] public bool[] accrUsed;
+        [HideInInspector] public Color[] colors;
+        [HideInInspector] public Transform accr1;
+        [HideInInspector] public Transform accr2;
+        [HideInInspector] public Transform accr3;
+        [HideInInspector] public Transform accr4;
+        public Player playerPrefab;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            accrUsed = new bool[4];
+            accrUsed = new[] { false, false, false, false };
+            colors = new Color[4];
+            colors = new []{ Color.yellow, Color.red, Color.green, Color.gray };
+            print("type : " + colors.GetType());
+            accr1 = transform.Find("Accroche1");
+            accr2 = transform.Find("Accroche2");
+            accr3 = transform.Find("Accroche3");
+            accr4 = transform.Find("Accroche4");
+        }
+
+        private void Start()
+        {
+            Player player1 = Instantiate<Player>(playerPrefab, transform);
+            player1.board = this;
+            Player player2 = Instantiate<Player>(playerPrefab, transform);
+            player2.board = this;
+            Player player3 = Instantiate<Player>(playerPrefab, transform);
+            player3.board = this;
+            Player player4 = Instantiate<Player>(playerPrefab, transform);
+            player4.board = this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Unity/Assets/Code/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code
+{
+    public class Player : MonoBehaviour
+    {
+        public Board board;
+        public SpriteRenderer sr;
+        // Start is called before the first frame update
+        void Start()
+        {
+            int randomPlacement;
+            do
+            {
+                randomPlacement = Random.Range(1, 5);
+            }
+            while (board.accrUsed[randomPlacement - 1]);
+            board.accrUsed[randomPlacement - 1] = true;
+            string name = "Accroche" + randomPlacement;
+            print(name);
+            Transform t = board.transform.Find(name).transform;
+            transform.position = t.position;
+            transform.rotation = t.rotation;
+            transform.localScale = new Vector3(transform.localScale.x * 0.1f, transform.localScale.y * 0.1f, 0);
+            
+            do
+            {
+                randomPlacement = Random.Range(0, 4);
+            }
+            while (board.colors[randomPlacement] == Color.white);
+            sr.color = board.colors[randomPlacement];
+            print(sr.color.ToString());
+            board.colors[randomPlacement] = Color.white;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
