e2284c787a44fe7950a107afd8a55d065a3b2b72
Brookener <46428517+Brookener@users.noreply.github.com>
2019-07-12 14:31:27 +0200

    update fmod et sound class

--- Unity/Assets/Code/Sound.cs
+++ Unity/Assets/Code/Sound.cs
@@ -12,9 +12,11 @@ public static class Sound
     static EventInstance langueImpact;
     static EventInstance barkIdle;
     static EventInstance gobe;
-    static EventInstance Bonus;
-    static EventInstance Victoire;
+    static EventInstance bonus;
+    static EventInstance malus;
+    static EventInstance victoire;
     static EventInstance gameplayMusic;
+    static EventInstance menuScreenEvent;
 
     public static void LoadSounds()
     {
@@ -22,9 +24,11 @@ public static class Sound
         langueImpact = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Langue_Impact");
         barkIdle = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/BARK_Idle");
         gobe = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Gobe");
-        Bonus = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Bonus");
-        Victoire = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Victoire");
+        bonus = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Bonus");
+        victoire = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Victoire");
         gameplayMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Musiques/Gameplay_Music");
+        menuScreenEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Musiques/Menu_Music");
+        malus = FMODUnity.RuntimeManager.CreateInstance("event:/Gameplay_SFX/SFX_Malus");
 
 
 
@@ -32,6 +36,7 @@ public static class Sound
 
     public static void LangueCollisionBegin()
     {
+        langueImpact.start();
         langueCollision.start();
     }
 
@@ -42,20 +47,23 @@ public static class Sound
 
     public static void GameStart()
     {
+        menuScreenEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         gameplayMusic.start();
         barkIdle.start();
     }
 
-    public static void VictoireScreen()
+    public static void VictoireScreen(int perso)
     {
         barkIdle.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         gameplayMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        Victoire.start();
+        victoire.setParameterByName("Perso", perso, true);
+        victoire.start();
     }
 
     public static void MenuScreen()
     {
-        Victoire.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        victoire.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        menuScreenEvent.start();
 
     }
     
@@ -63,6 +71,18 @@ public static class Sound
     {
         gobe.setParameterByName("Perso", perso, true);
         gobe.start();
+    }
+
+    public static void Bonus(int perso)
+    {
+        bonus.setParameterByName("Perso", perso);
+        bonus.start();
+    }
+
+    public static void Malus(int perso)
+    {
+        malus.setParameterByName("Perso", perso);
+        malus.start();
     }
 
 
